bb35624b6830d5c844cb5be5afbc532344b23a07
pedro lima <pedro.lima.wk@gmail.com>
2022-03-29 16:31:44 -0300

    Feat: Iniciando questao 3

---
+++ Questao3/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao3
+{
+    public class Funcionario
+    {
+        public Funcionario(string name, string role, int idade)
+        {
+            Name = name;
+            Role = role;
+            Idade = idade;
+        }
+
+        public string Name { get; set; }
+        public string Role { get; set;}
+        public int Idade { get; set; }
+
+
+    }
+}
--- Questao3/Program.cs
+++ Questao3/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Questao3
 {
     internal class Program
     {
+
+        public static List<Funcionario> ListaFuncionarios;
         static void Main(string[] args)
         {
-            Console.WriteLine("Questao 3");
+            var arrayFuncionarios = new Funcionario[] 
+            { 
+                new Funcionario("Givaldo", "Mendigo", 48),
+                new Funcionario("Renílson", "Pedreiro", 40),
+                new Funcionario("Luiza", "Médica", 25),
+                new Funcionario("Bruno", "Desenvolvedor", 23),
+                new Funcionario("Casseb", "Empresário", 40),
+                new Funcionario("Fabinho", "CEO", 46),
+                new Funcionario("Shirley", "Gerente", 40),
+                new Funcionario("Tomé", "Brincante", 23),
+                new Funcionario("Gabriel", "GP", 25),
+                new Funcionario("Neymar", "Jogador", 30),
+            };
+
+
+
+            
+        }
+
+        private void PopulaListaFuncionarios()
+        {
+
         }
     }
 }
